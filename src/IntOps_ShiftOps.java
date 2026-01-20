1387f8913b30a5f4838c1c796167ff34aab5e972
xyc0833 <2031362123@qq.com>
2026-01-20 15:35:46 +0800

    更新相关代码

--- src/IntOps_ShiftOps.java
+++ src/IntOps_ShiftOps.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class IntOps_ShiftOps {
 	public static void main(String[] args) {
@@ -20,6 +21,39 @@ public class IntOps_ShiftOps {
 		System.out.println(a3);
 		int a4 = a3 << 23;
 		System.out.println(a4);
+		
+		char b = 'b';
+		String s = "hello";
+		System.out.println(s);
+		s = s + "world";
+		System.out.println(s);
+		
+		int [] ns = new int[5];
+		System.out.println(ns.length);
+		
+		/**
+		 * 练习
+		计算前N个自然数的和可以根据公示
+		(1 + N) * N
+		请根据公式计算前N个自然数的和
+		 */
+		//创建一个 scanner用于接受输入
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("请输入正整数");
+		int k = sc.nextInt();
+		String nameString = sc.nextLine();
+		if(k < 0) {
+			System.out.println("输入必须为正整数！");
+		}else {
+			long sum = (1 + k) * k / 2;
+			System.out.println("前 " + k + " 个自然数的和是：" + sum);
+		}
+		sc.close();
+		int abc = k >=0 ? a : -a;
+		System.out.println(abc);
+		
+
 		
 	}
 }
